7e6239f8d41a99c6200de6887698b0edd20f9bde
Dmitriy19815 <fdv.nipi@gmail.com>
2017-06-05 12:04:05 +0300

    Explanations and comments included

--- src/main/java/pro/codecare/java/GeneralLogicTests.java
+++ src/main/java/pro/codecare/java/GeneralLogicTests.java
@@ -7,7 +7,10 @@ import java.io.IOException;
 import java.util.Random;
 
 /**
- * This is test application for deep checking logic of functions
+ * This is test application for deep checking logic of functions.
+ *
+ * Checked the results for concurrent matrix calculations in asynchronous and synchronous thread concepts
+ * and compares results of them with default implementation of matrix multiply calculation.
  */
 public class GeneralLogicTests {
 
@@ -15,18 +18,30 @@ public class GeneralLogicTests {
     int dim = 10 + (new Random()).nextInt(99);
 
     try {
+      /**
+       * First simple matrix for calculations
+       */
       Matrix mtr1 = new Matrix(10 + (new Random()).nextInt(999), dim);
       System.out.println("First (left) matrix: "+mtr1.toString());
 
+      /**
+       * Second simple matrix for calculations
+       */
       Matrix mtr2 = new Matrix(dim, 10 + (new Random()).nextInt(999));
       System.out.println("Second (right) matrix: "+mtr2.toString());
 
+      /**
+       * Matrix for compare results of calculations by default method
+       */
       Matrix mtrDefault = new Matrix();
       mtrDefault.Multiplication(mtr1, mtr2);
       System.out.println("Multiply with NO concurrent (default) algirithm for matrix " + mtrDefault.toString());
       printResults("Multiply.NOconcurrent.results.log", mtr1, mtr2, mtrDefault);
       System.out.println("\tOutput file was generated\n");
 
+      /**
+       * Matrix for synchronous thread concept of calculations
+       */
       Matrix mtrSync = new Matrix();
       mtrSync.MultiplicationConcurrent(mtr1, mtr2, 0);
       System.out.println("Multiply in sync concurrency algirithm for matrix " + mtrSync.toString());
@@ -34,6 +49,9 @@ public class GeneralLogicTests {
       printResults("Multiply.concurrent0.results.txt", mtr1, mtr2, mtrSync);
       System.out.println("\tOutput file was generated\n");
 
+      /**
+       * Compare results of default and synchronous thread concept of calculations
+       */
       for (int row = 0; row < mtr1.getRows(); ++row) {
         for (int col = 0; col < mtr2.getColumns(); ++col) {
           if (mtrDefault.getValue(row, col) != mtrSync.getValue(row, col)) {
@@ -43,13 +61,18 @@ public class GeneralLogicTests {
         }
       }
 
+      /**
+       * Matrix for asynchronous thread concept of calculations
+       */
       Matrix mtrAsync = new Matrix();
       mtrAsync.MultiplicationConcurrent(mtr1, mtr2, 1);
       System.out.println("Multiply in async concurrency algirithm for matrix " + mtrAsync.toString());
       printResults("Multiply.concurrent1.results.txt", mtr1, mtr2, mtrAsync);
       System.out.println("\tOutput file was generated\n");
 
-
+      /**
+       * Compare results of default and asynchronous thread concept of calculations
+       */
       for (int row = 0; row < mtr1.getRows(); ++row) {
         for (int col = 0; col < mtr2.getColumns(); ++col) {
           if (mtrDefault.getValue(row, col) != mtrAsync.getValue(row, col)) {
@@ -67,12 +90,12 @@ public class GeneralLogicTests {
 
 
   /**
-   * Вывод трёх матриц в файл
+   * Output into file stream results of calculation
    *
-   * @param fileName      Имя файла для вывода
-   * @param first         Первая матрица
-   * @param second        Вторая матрица
-   * @param result        Результирующая матрица
+   * @param fileName
+   * @param first         First matrix object
+   * @param second        Second matrix object
+   * @param result        Destination (result) matrix object
    */
   private static void printResults(final String fileName, Matrix first, Matrix second, Matrix result) {
 
